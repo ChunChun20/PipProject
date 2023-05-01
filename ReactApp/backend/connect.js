3a37b992fae5ed307e139a2f50a57b58d67fa181
Chun <Vivy1234423@gmail.com>
2023-05-01 22:47:19 +0300

    updated the database so its hosted online now instead of locally

--- ReactApp/backend/connect.js
+++ ReactApp/backend/connect.js
@@ -3,9 +3,20 @@ import mysql from "mysql"
 
 //connecting to the database
 
+// export const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"password",
+//     database:"pip"
+// })
+
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"pip"
+    host:"sql7.freesqldatabase.com",
+    user:"sql7615325",
+    password:"Dni9VjAQZV",
+    database:"sql7615325"
 })
+
+
+
+//jdbc:mysql://sql7.freesqldatabase.com:3306/sql7615325
